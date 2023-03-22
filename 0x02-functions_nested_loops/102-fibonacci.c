ba113678a97cf9ebefaa88c2c01bc07ef7838e4b
Slye-Man <selaelosebola67@gmail.com>
2023-03-21 19:41:30 -0700

    101 natural.c 102 fibonacci.c 103 fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+/**
+ * main - Entry point
+ *
+ * Description: prints the first 50 Fibonacci numbers
+ *		starting with 1 and 2 folled by a new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int counter;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+
+	for (counter = 0; counter < 50; counter++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (counter == 49)
+			printf("\n");
+		else
+			printf(', ');
+	}
+
+	return (0);
+}

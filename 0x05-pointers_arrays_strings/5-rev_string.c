fbbde3332aea3a9841740320b32cc3e73d1e0560
Slye-Man <selaelosebola67@gmail.com>
2023-03-28 04:38:09 -0700

    Pointers, arrays and string: intermediate

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string parameter input
+ *
+ * Return: Nothing
+*/
+
+void rev_string(char *s)
+{
+	int i, j;
+	char c;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	for (j = 0; j < i / 2; j++)
+	{
+		c = s[j];
+		s[j] = s[i - 1 - j];
+		s[i - 1 - j] = c;
+	}
+}

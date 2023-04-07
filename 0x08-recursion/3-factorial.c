908e0ec8784c7887e1141c78525db2e08529588a
Slye-Man <selaelosebola67@gmail.com>
2023-04-06 19:41:00 -0700

    Recursions - intermediate

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * @n: int
+ * Return: int
+ */
+
+int factorial(int n)
+{
+	if (n < 0)		/*error -1 if number is negative*/
+		return (-1);
+	else if (n == 0)	/*return factorial of 0 = 1*/
+		return (1);
+	else			/*do factorial of n if its not less or equal to 0*/
+		return (n * factorial(n - 1));
+}
